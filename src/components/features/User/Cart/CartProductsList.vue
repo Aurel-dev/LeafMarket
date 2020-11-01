41459ffec2f7aa06e25c116551c5c4367828e6e6
aurelien dhellemme <dhellemme.a@gmail.com>
2020-11-01 23:00:16 +0100

    logo

--- src/components/features/User/Cart/CartProductsList.vue
+++ src/components/features/User/Cart/CartProductsList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="d-flex flex-column p-5">
+  <img src="https://img2.freepng.fr/20180528/gf/kisspng-shopping-bags-trolleys-shopping-cart-logo-clip-a-online-shop-5b0c34bb57cc75.7415233215275265873596.jpg" height="280">
   <h3 class="border-bottom pb-3 text-center">Mon Panier</h3>
     <template v-if="cart.length">
       <cart-product-item v-for="item in cart" :key="item.id" :item="item"></cart-product-item>
